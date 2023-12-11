08bc8f17a04ca1f133f78392ad42f2ad3b5bc434
Lukas Passolt <lukas.passolt@adesso-service.com>
2023-12-11 16:20:51 +0100

    fix location

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "main" {
   name     = "platform-rg"
-  location = "West Europe"
+  location = "Germany West Central"
 }
 
 resource "azurerm_virtual_network" "main" {
   name                = var.virtual_network_name
-  location            = "West Europe"
+  location            = "Germany West Central"
   resource_group_name = azurerm_resource_group.main.name
   address_space       = [var.ip_address_range]
 }
